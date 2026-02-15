979d3999c54de47609d1ceea1c8b080a483a52eb
Tiffany Simpson <simpsontiffany84@gmail.com>
2026-02-15 17:34:43 -0500

    feat: implement filterSkillsByCategory (green)

--- skillswap-functions.js
+++ skillswap-functions.js
@@ -1,1 +1,12 @@
-module.exports = {};
+function filterSkillsByCategory(skills, category) {
+    if (!Array.isArray(skills)) return [];
+
+    if (category === "All") return skills;
+
+    return skills.filter((skill) => skill.category === category);
+}
+
+module.exports = {
+    filterSkillsByCategory,
+};
+
